258ec0be9f79fd2bc4ca454c778d70373940096a
AayushNarwade <aayushnarwade10@gmail.com>
2025-11-09 17:34:56 +0530

    Upgrade Groq model to llama-3.3-70b-versatile

--- parent_agent.py
+++ parent_agent.py
@@ -8,7 +8,8 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 client = Groq(api_key=GROQ_API_KEY)
-MODEL_NAME = "llama-3.1-70b-versatile"
+MODEL_NAME = "llama-3.3-70b-versatile"
+
 
 
 app = Flask(__name__)
